05491b9e4566990eac7093fac9ee99b31a65d717
silvergjeka22 <86209628+silvergjeka22@users.noreply.github.com>
2024-05-11 13:19:27 +0200

    makong the save result visit works

--- src/main/java/it/hospitium/controller/medico/MedicoController.java
+++ src/main/java/it/hospitium/controller/medico/MedicoController.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.*;
@@ -66,7 +68,36 @@ public class MedicoController {
         // Add attributes
         model.addAttribute("appointment", appointment);
 
-        return "/medico/appointment-details"; // Return the HTML page to display appointment details
+        return "/medico/appointment-details";
     }
 
+    @PostMapping("/medico/appointment/saveResult/{id}")
+    public String saveResult(@PathVariable Long id,
+            @RequestParam("resultNote") String resultNote,
+            RedirectAttributes redirectAttributes) {
+        // Retrieve the appointment by id
+        Optional<Appointment> maybeAppointment = appointmentRepository.findById(id);
+        if (maybeAppointment.isEmpty()) {
+            return "redirect:/medico/home";
+        }
+
+        // Get the appointment
+        Appointment appointment = maybeAppointment.get();
+        // get the visit type
+        Visita.VisitType visitType = Visita.VisitType.valueOf(appointment.getVisitType().name());
+
+        // Create a new visit details
+        Visita newVisit = new Visita(appointment.getData(),
+                resultNote,
+                visitType,
+                appointment.getUrgenza(),
+                appointment.getMedico(),
+                appointment.getPatient(),
+                null); // Nurse is set to null
+
+        // Save the new visit
+        visitaRepository.save(newVisit);
+
+        return "redirect:/medico/home";
+    }
 }
