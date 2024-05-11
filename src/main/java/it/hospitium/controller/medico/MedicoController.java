06307dd93e7138ff58de1e318b6c11e23acbd07f
silvergjeka22 <86209628+silvergjeka22@users.noreply.github.com>
2024-05-11 12:23:30 +0200

    adding the appointment details page

--- src/main/java/it/hospitium/controller/medico/MedicoController.java
+++ src/main/java/it/hospitium/controller/medico/MedicoController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.*;
@@ -43,13 +44,29 @@ public class MedicoController {
         List<Appointment> appointments = (List<Appointment>) appointmentRepository.findByMedico(medico);
 
         // Add attributes
-        model.addAllAttributes(
-                Map.of(
-                        "user", medico,
-                        "breadcrumbs", List.of(new Breadcrumb("home", "/medico/home")),
-                        "appointments", appointments));
+        model.addAttribute("user", medico);
+        model.addAttribute("breadcrumbs", List.of(new Breadcrumb("home", "/medico/home")));
+        model.addAttribute("appointments", appointments);
+        model.addAttribute("categories", Visita.getVisitCategories());
 
         return "/medico/home";
+    }
+
+    @GetMapping("/medico/appointment/{id}")
+    public String viewAppointmentDetails(@PathVariable Long id, Model model) {
+        // Retrieve the appointment by id
+        Optional<Appointment> maybeAppointment = appointmentRepository.findById(id);
+        if (maybeAppointment.isEmpty()) {
+            return "redirect:/medico/home";
+        }
+
+        // Get the appointment
+        Appointment appointment = maybeAppointment.get();
+
+        // Add attributes
+        model.addAttribute("appointment", appointment);
+
+        return "/medico/appointment-details"; // Return the HTML page to display appointment details
     }
 
 }
