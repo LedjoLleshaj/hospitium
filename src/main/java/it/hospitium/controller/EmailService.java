d68557bb8b155909ef492818a205ec5c0e6a8726
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2024-05-14 17:18:13 +0200

    fixed email settings

--- src/main/java/it/hospitium/controller/EmailService.java
+++ src/main/java/it/hospitium/controller/EmailService.java
@@ -2,6 +2,7 @@ package it.hospitium.controller;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +14,11 @@ public class EmailService {
         this.emailSender = emailSender;
     }
 
+    @Async
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
+        message.setFrom("mailtrap@demomailtrap.com");
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
